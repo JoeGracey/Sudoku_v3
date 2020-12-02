88f4a1070960035603009ea36f9eaa865f0e0e43
Joe Gracey <joe.gracey@gmail.com>
2020-12-02 15:28:19 -0700

    isItValid

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        int userGrid[][] = new int[][] { // [horizontal][vertical]
+        printTheGrid();
+    }
+
+
+    public static class sudokuGrid {
+        static int userGrid[][] = new int[][] { // [horizontal][vertical]
                 {0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -13,18 +18,27 @@ public class Main {
                 {0, 0, 0, 0, 0, 0, 0, 0, 0}
         };
 
-        int experimentsGrid[][] = new int[9][9];
-        String posibilitiesGrid[][] = new String[9][9];
-        print(experimentsGrid);
+        static int experimentGrid[][] = new int[9][9];
+        //static String posibilitiesGrid[][] = new String[9][9];
+        printTheGrid(experimentGrid);
     }
 
-    public static void print(int[][] grid) {
+
+    public static void printTheGrid(int[][] blah) { // "blah" can be called anything
         System.out.println(" "); // Creates some room at the top
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                System.out.print(grid[i][j]);
+        for (int i = 0; i < 9; i++) { // How many rows
+            for (int j = 0; j < 9; j++) { // How many numbers per row
+                System.out.print(blah[i][j] + " ");
             }
             System.out.println("*"); // Returns after each row is printed
         }
+    }
+
+    public static boolean isItValid(int x, int y, int[][] bleep) {
+        String temp = " ";
+        for(int i = 0; i < 9; i++) {
+            temp += bleep[i][y]; // horizontal
+        }
+        return true;
     }
 }
