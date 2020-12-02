1e0048b088f025f3eb920bd063be2252ed98310e
Joe Gracey <joe.gracey@gmail.com>
2020-12-02 10:52:17 -0700

    grid

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("TEST");
-        System.out.println("TEST");
+        int userGrid[][] = new int[][] { // [horizontal][vertical]
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        int experimentsGrid[][] = new int[9][9];
+        String posibilitiesGrid[][] = new String[9][9];
+        print(experimentsGrid);
+    }
+
+    public static void print(int[][] grid) {
+        System.out.println(" "); // Creates some room at the top
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(grid[i][j]);
+            }
+            System.out.println("*"); // Returns after each row is printed
+        }
     }
 }
